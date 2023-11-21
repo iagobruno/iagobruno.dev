c63bf8bf24ec5b35c0307a8f6f01d6ed3794b53f
Iago Bruno <iagobruno.dev@gmail.com>
2023-11-21 09:30:01 -0300

    Fix phone number

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -18,7 +18,7 @@ export default function Footer () {
           <div className="text-4xl font-semibold mb-5">Contato</div>
           <div className="text-lg flex gap-x-5 gap-y-1 justify-center items-center flex-wrap mx-auto">
             <a href="mailto:iagobruno.dev@gmail.com">Email</a>
-            <a href="https://wa.me/558897174798">WhatsApp</a>
+            <a href="https://wa.me/558897174708">WhatsApp</a>
             <a href="https://github.com/iagobruno">GitHub</a>
             <a href="https://instagram.com/iagotico">Instagram</a>
             <a href="https://www.linkedin.com/in/iagobruno">LinkedIn</a>
