b4d2789a3fb09f58442e0e6b859a222817904997
Iago Bruno <iagobruno.dev@gmail.com>
2023-11-26 05:15:44 -0300

    Add navigation loading bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import ProgressBar from 'nextjs-toploader'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="blurred-background" />
         {children}
+        <ProgressBar />
       </body>
     </html>
   )
