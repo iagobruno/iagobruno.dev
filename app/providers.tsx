a8471a0cd99e1c5a9fb7ce053d83d5fc8d013b8b
Iago Bruno <iagobruno.dev@gmail.com>
2025-04-29 17:45:30 -0300

    feat: Implement smooth scroll

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,19 @@
 'use client'
 import { ThemeProvider } from 'next-themes'
-import type { PropsWithChildren } from 'react'
+import { useEffect, type PropsWithChildren } from 'react'
+import Lenis from 'lenis'
 
 export function Providers({ children }: PropsWithChildren) {
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      autoRaf: true,
+    })
+    return () => {
+      lenis.destroy()
+    }
+  }, []);
+
   return (
     <ThemeProvider attribute="class">
       {children}
