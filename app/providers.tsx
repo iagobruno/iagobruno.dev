6863cd6f733654ac758d2bc4b45aa1862ea17370
Iago Bruno <iagobruno.dev@gmail.com>
2025-12-16 16:53:40 -0300

    feat: Install PostHog

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from 'next-themes'
 import { useEffect, type PropsWithChildren } from 'react'
 import useMount from 'react-use/esm/useMount'
 import Lenis from 'lenis'
+import posthog from 'posthog-js'
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
 export function Providers({ children }: PropsWithChildren) {
 
@@ -14,10 +16,22 @@ export function Providers({ children }: PropsWithChildren) {
       lenis.destroy()
     }
   });
+  
+  useMount(() => {
+    if (process.env.NODE_ENV != 'development') {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+        person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+        defaults: '2025-11-30'
+      })
+    }
+  });
 
   return (
     <ThemeProvider attribute="class">
-      {children}
+      <PHProvider client={posthog}>
+        {children}
+      </PHProvider>
     </ThemeProvider>
   )
 }
